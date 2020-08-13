f2f010332d9c985a6a16bcc27c692193df89da55
thibaultChatelain <thibault.chatelain@etu.univ-lyon1.fr>
2020-08-13 13:11:11 +0200

    Youtube-dl: download and send file

--- server/app.py
+++ server/app.py
@@ -1,13 +1,10 @@
-app_defaults = {
+defaults = {
     'FORMAT': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',
-    'AUDIO_FORMAT': None,
-    'AUDIO_QUALITY': '192',
-    'OUTPUT': '/youtube-dl/%(title)s [%(id)s].%(ext)s',
-    'YDL_SERVER_HOST': '0.0.0.0',
-    'PORT': 5000,
+    'OUTPUT': '/youtube-dl/%(title)s.%(ext)s',
 }
 
-from flask import Flask
+import youtube_dl
+from flask import Flask, request, send_file
 from redis import Redis
 
 app = Flask(__name__)
@@ -17,6 +14,24 @@ redis = Redis(host='redis', port=6379)
 def hello():
     redis.incr('hits')
     return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
+
+@app.route('/youtube_dl/q', methods = ['POST'])
+def call():
+    url = request.json.get("url")
+    options = {
+        'format': request.json.get("format")
+    }
+
+    if not url:
+        return {"error": "No url provided"}
+
+    download(url)
+    return send_file("/code/Little Wing-Rj_NUS9hwxA.webm", attachment_filename="test", as_attachment=True)
+    #return send_file("/code/Little Wing-Rj_NUS9hwxA.webm")
+
+def download(url):
+    with youtube_dl.YoutubeDL() as ydl:
+        ydl.download([url])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
